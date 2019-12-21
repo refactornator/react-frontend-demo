fb36d283c8ccd145fd26c4f78555c2ec4076b241
William Lindner <wlindner522@gmail.com>
2019-12-20 23:56:39 -0800

    Print useful output

--- getLatestVersion.sh
+++ getLatestVersion.sh
@@ -4,4 +4,6 @@ ls cool.william.frontend || wget https://plugins.gradle.org/plugin/cool.william.
 
 LATEST_VERSION="$(sed -nE 's/.*Version (0.0.[0-9]+) \(latest\).*/\1/p' cool.william.frontend)"
 
+echo "Latest Version: $LATEST_VERSION"
+
 echo "frontendVersion=$LATEST_VERSION" > "gradle.properties"
